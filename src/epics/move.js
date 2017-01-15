2bc31f5386d915f58d6c72cfcc4394f614197ea3
Matt Glover <matthewglover@me.com>
2017-01-15 21:12:46 +0000

    feat(computer): randomly select computer's first move on empty board
    no winning strategy, so no need to run mini-max

--- src/epics/move.js
+++ src/epics/move.js
@@ -1,6 +1,7 @@
 import { compose } from 'redux';
 import * as fromReducer from '../reducers';
 import miniMax from '../logic/mini_max';
+import makeMove from '../board/make_move';
 import * as fromActionCreators from '../action_creators';
 import {
   any,
@@ -9,10 +10,27 @@ import {
   isSetBoardAction } from './helpers';
 
 
-const getNextBoard = store => () => {
-  const { board } = store.getState();
+const calcNextBoard = (board) => {
   const [, nextBoard] = miniMax(board);
   return nextBoard;
+};
+
+const chooseRandom = (board) => {
+  const randomVal = Math.floor(Math.random() * 10);
+  return makeMove(board, 1, randomVal);
+};
+
+const isEmptyBoard = board =>
+  board.every(status => status === 0);
+
+const getNextBoard = store => () => {
+  const { board } = store.getState();
+
+  const res = (isEmptyBoard(board)
+    ? chooseRandom(board)
+    : calcNextBoard(board));
+
+  return res;
 };
 
 const getActivePlayerType = store => () =>
