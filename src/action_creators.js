cae51cfb9529ab926e01504081646c51e4f3331d
Matt Glover <matthewglover@me.com>
2017-03-03 17:38:08 +0000

    refactor(action_creators): add Flow types to action creators

--- src/action_creators.js
+++ src/action_creators.js
@@ -1,23 +1,24 @@
+// @flow
 
-export const move = position =>
+export const move = (position: number) =>
   ({
     type: 'MOVE',
     position,
   });
 
-export const setNewGame = gameType =>
+export const setNewGame = (gameType: GameType) =>
   ({
     type: 'SET_NEW_GAME',
     gameType,
   });
 
-export const setBoard = board =>
+export const setBoard = (board: Board) =>
   ({
     type: 'SET_BOARD',
     board,
   });
 
-export const setGameCompleted = completed =>
+export const setGameCompleted = (completed: boolean) =>
   ({
     type: 'SET_GAME_COMPLETED',
     completed,
