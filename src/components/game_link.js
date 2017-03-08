66215674528d780e36d5d28328465887de198203
Matt Glover <matthewglover@me.com>
2017-03-08 12:07:22 +0000

    feat(ui): restyle home and game over routes

--- src/components/game_link.js
+++ src/components/game_link.js
@@ -1,5 +1,6 @@
 // @flow
 import React from 'react';
+import { Button } from 'rebass';
 import { Link } from 'react-router-dom';
 
 type Props = {
@@ -7,16 +8,25 @@ type Props = {
   description: string,
 }
 
+const style = {
+  button: {
+    width: '200px',
+    textAlign: 'center',
+  },
+};
+
 const GameLink = ({ gameType, description }: Props) =>
-  <li>
-    <Link
-      to={{
-        pathname: '/game',
-        query: { gameType },
-      }}
-    >
-      {description}
-    </Link>
-  </li>;
+  <Button
+    to={{
+      pathname: '/game',
+      query: { gameType },
+    }}
+    is={Link}
+    backgroundColor="white"
+    color="LightSlateGray"
+    style={style.button}
+  >
+    {description}
+  </Button>;
 
 export default GameLink;
