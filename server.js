6caf43ef9a330875509477c189d8ce683a718dd9
Matt Glover <matthewglover@me.com>
2017-03-08 09:31:04 +0000

    build(webpack): update webpack configurations to serve js from /public
    update server.js to reply with index.html for all routes except /public

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const Boom = require('boom');
 const {
   createServer,
   setConnection,
@@ -5,23 +6,41 @@ const {
   addRoutes,
   startServer } = require('@matthewglover/hapi-wrapper');
 
+
 const routeOptions = {
   setStaticRoutes: true,
-  path: '/{param*}',
+  path: '/public/{param*}',
   handler: {
     directory: {
-      path: './dist',
+      path: './dist/public',
       listing: false,
     },
   },
 };
+
+const routes = [
+  {
+    method: 'GET',
+    path: '/{params*}',
+    handler(req, reply) {
+      reply.file('dist/public/index.html');
+    },
+  },
+  {
+    method: 'GET',
+    path: '/public/index.html',
+    handler(req, reply) {
+      reply(Boom.notFound());
+    },
+  },
+];
 
 const port = process.env.PORT || 4000;
 
 createServer()
   .then(setConnection({ port }))
   .then(registerPlugins())
-  .then(addRoutes([], routeOptions))
+  .then(addRoutes(routes, routeOptions))
   .then(startServer)
   /* eslint-disable no-console */
   .then(server => console.log(`Server running at: ${server.info.uri}`))
