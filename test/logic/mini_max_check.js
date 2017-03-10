3e444832b6339a4ecd53f2fc4ec8d27479fae262
Matt Glover <matthewglover@me.com>
2017-03-10 11:10:45 +0000

    test(miniMax): update miniMax check just to check result for each starting position
    algorithm should always make the same move given particular board state
    
    so no need to run random
    
    tests

--- test/logic/mini_max_check.js
+++ test/logic/mini_max_check.js
@@ -2,9 +2,6 @@ const test = require('tape');
 const miniMax = require('../../src/logic/mini_max').default;
 const { EMPTY_BOARD, isCompleteBoard, getWinningLines, makeMove } = require('../../src/board');
 
-
-const randomPosition = () => Math.floor(Math.random() * 9);
-
 const getNextBoard = (board) => {
   const [, nextBoard] = miniMax(board);
   return nextBoard;
@@ -18,10 +15,9 @@ const runGame = board =>
 
 
 test('miniMax (logic)', (t) => {
-  Array.from({ length: 20 }).forEach((_, i) => {
-    const startingMove = randomPosition();
-    const result = runGame(makeMove(EMPTY_BOARD, 1, startingMove));
-    t.equal(getWinningLines(result).length, 0, `Game ${i}, starting move: ${startingMove} - is a draw`);
+  Array.from({ length: 9 }).forEach((_, i) => {
+    const result = runGame(makeMove(EMPTY_BOARD, 1, i));
+    t.equal(getWinningLines(result).length, 0, `Game ${i}, starting move: ${i} - is a draw`);
   });
   t.end();
 });
